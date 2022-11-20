6e83798da7157843d86a871e2c5c0916dcaec923
Yu-Fu Fu <yufu@yfu.tw>
2022-11-20 02:33:47 -0500

    [misc] config and log

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
+use anyhow::Result;
+use once_cell::sync::OnceCell;
+use serde::{Deserialize, Serialize};
+use tracing::{debug, info};
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+
+static APP_NAME: OnceCell<&str> = OnceCell::new();
+
 shadow_rs::shadow!(build);
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Default, Debug, Serialize, Deserialize)]
+struct Config {
+    api_token: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+    APP_NAME.set("maskedmail-rs").unwrap();
+    let cfg: Config = confy::load(APP_NAME.get().unwrap(), None)?;
+    tracing::debug!(?cfg);
+    Ok(())
 }
