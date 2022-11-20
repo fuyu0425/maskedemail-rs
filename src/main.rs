6708bc0ccdeb1ff013987477fe1f1582190fb2ab
Yu-Fu Fu <yufu@yfu.tw>
2022-11-20 03:26:25 -0500

    [misc] some global variables and TODO

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,32 @@ struct Config {
     api_token: String,
 }
 
+static CONFIG: OnceCell<Config> = OnceCell::new();
+
+fn get_api_token() -> &'static String {
+    &CONFIG.get().unwrap().api_token
+}
+
+static CLIENT: OnceCell<reqwest::Client> = OnceCell::new();
+
+fn get_client() -> &'static reqwest::Client {
+    &CLIENT.get().unwrap()
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::registry()
         .with(fmt::layer())
         .with(EnvFilter::from_default_env())
         .init();
-    APP_NAME.set("maskedmail-rs").unwrap();
-    let cfg: Config = confy::load(APP_NAME.get().unwrap(), None)?;
+    APP_NAME.set("maskedmail").unwrap();
+    let cfg: Config = confy::load(APP_NAME.get().unwrap(), "config")?;
+    let file = confy::get_configuration_file_path(APP_NAME.get().unwrap(), "config")?;
     tracing::debug!(?cfg);
+    tracing::debug!(?file);
+    CONFIG.set(cfg).unwrap();
+    tracing::debug!("{}", get_api_token());
+    let client = reqwest::Client::new();
+    CLIENT.set(client).unwrap();
     Ok(())
 }
